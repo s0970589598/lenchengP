b5558743a3900e3a106c66685b24e0a17c99fc8f
st92308 <st92308@gmail.com>
2024-03-13 16:09:22 +0800

    sourceTypeParam

--- app/Http/Controllers/MarkdownController.php
+++ app/Http/Controllers/MarkdownController.php
@@ -183,8 +183,14 @@ class MarkdownController extends Controller
                 $filteredFiles = array_filter($files, function($file) use ($sourceTypeParam) {
                     // 取得檔案的檔名（不包含副檔名）
                     $fileName = pathinfo($file->getFilename(), PATHINFO_FILENAME);
-                    return strpos($fileName, $sourceTypeParam) !== false;
 
+                    if ($sourceTypeParam != 'partner'){
+                        return strpos($fileName, $sourceTypeParam) !== false;
+
+                    } else {
+                        return (strpos($fileName, $sourceTypeParam) !== false && ! strpos($fileName, 'partner'));
+
+                    }
                 });
             }
             // array_multisort($filesize,SORT_DESC,SORT_NUMERIC, $filteredFiles);//按大小排序          
